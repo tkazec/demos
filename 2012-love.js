b76e7b7c94d531e7b85d6d21ad2508bb4ecf9f75
Teddy Cross <tkazec@gmail.com>
2012-02-09 23:50:32 -0500

    Help the compiler along a bit
    1009 bytes!

--- 2012-love.js
+++ 2012-love.js
@@ -1,29 +1,35 @@
 // "Firehearts", for js1k 2012 (love) by @tkazec | <url>
 // b = body, c = cvs, a = ctx
-// CC checklist (advanced): revert animFrame changes, rm newlines, rm last semicolon
+// CC checklist (advanced): revert animFrame changes, rm newlines, rm last semicolon, add wrapper
+
+var w = this,
+	r = "equestAnimationFrame",
+	animFrame = w["r"+r] || w["webkitR"+r] || w["mozR"+r] || w["msR"+r] || w["oR"+r] || function(cb){ setTimeout(cb, 1000 / 60); };
+
+var MATH = Math,
+	PI = MATH.PI,
+	FLOOR = MATH.floor,
+	RAND = MATH.random;
 
 var WIDTH = c.width = 960,
 	HEIGHT = c.height = 540,
 	hanabi = [],
-	queue = 0,
-	w = this,
-	r = "equestAnimationFrame",
-	animFrame = w["r"+r] || w["webkitR"+r] || w["mozR"+r] || w["msR"+r] || w["oR"+r] || function(cb){ setTimeout(cb, 1000 / 60); };
+	queue = 0;
 
 function burst (x, y, size, speed, spread, count, delay) {
 	var list = [],
-		color = "#" + ["F20", "F0F", "F6B", "C18", "408", "800"][Math.floor(Math.random() * 6)];
+		color = "#" + ["F20", "F0F", "F6B", "C18", "408", "800"][FLOOR(RAND() * 6)];
 	
 	for (; count--;) {
-		var angle = Math.PI * 2 * Math.random(), velocity = speed * Math.random();
+		var angle = PI * 2 * RAND(), velocity = speed * RAND();
 		
 		list.push({
 			x: x, // x
 			y: y, // y
-			j: velocity * Math.cos(angle), // vx
-			k: velocity * Math.sin(angle), // vy
+			j: velocity * MATH.cos(angle), // vx
+			k: velocity * MATH.sin(angle), // vy
 			s: 0.97 + ((spread - 100) / 1000), // spread, 0-129
-			r: size + Math.floor(Math.random() * 6), // radius
+			r: size + FLOOR(RAND() * 6), // radius
 			c: color // color
 		});
 	}
@@ -39,22 +45,22 @@ function heart (x, y, s) {
 	a.beginPath();
 	a.moveTo(x - s1, y);
 	a.lineTo(x, y + s1);
-	a.arc(x + s2, y - s2, s3, 0.25 * Math.PI, 1.25 * Math.PI, true);
-	a.arc(x - s2, y - s2, s3, 1.75 * Math.PI, 0.75 * Math.PI, true);
+	a.arc(x + s2, y - s2, s3, 0.25 * PI, 1.25 * PI, true);
+	a.arc(x - s2, y - s2, s3, 1.75 * PI, 0.75 * PI, true);
 	a.fill();
 }
 
 (function render () {
 	if (!hanabi.length && !queue) {
-		for (var count = queue = Math.floor(Math.random() * 6); count--;) {
+		for (var count = queue = FLOOR(RAND() * 6); count--;) {
 			burst(
-				130 + Math.floor(Math.random() * 701), // x
-				130 + Math.floor(Math.random() * 151), // y
-				5 + Math.floor(Math.random() * 16), // size
-				4 + Math.floor(Math.random() * 7), // speed
-				70 + Math.floor(Math.random() * 40), // spread
-				50 + Math.floor(Math.random() * 201), // count
-				Math.floor(Math.random() * 180) // delay
+				130 + FLOOR(RAND() * 701), // x
+				130 + FLOOR(RAND() * 151), // y
+				5 + FLOOR(RAND() * 16), // size
+				4 + FLOOR(RAND() * 7), // speed
+				70 + FLOOR(RAND() * 40), // spread
+				50 + FLOOR(RAND() * 201), // count
+				FLOOR(RAND() * 180) // delay
 			);
 		}
 	}
@@ -66,7 +72,7 @@ function heart (x, y, s) {
 		h.k *= h.s; // vy*spread
 		h.y += 1.1; // y+gravity
 		
-		a.fillStyle = !Math.floor(Math.random() * 4) ? "rgba(256,256,256,.8)" : h.c;
+		a.fillStyle = !FLOOR(RAND() * 4) ? "rgba(256,256,256,.8)" : h.c;
 		heart(h.x, h.y, h.r);
 		
 		if ((h.r *= h.s) < 0.1) {

d293ffa3bd5c56a5ededfa63ce67ba4d92a3c0d4
Teddy Cross <tkazec@gmail.com>
2013-03-31 14:05:59 -0700

    flower warfare: death flower

--- js1k/flowerwarfare.js
+++ js1k/flowerwarfare.js
@@ -7,6 +7,8 @@ var bursts = [];
 var lives = 3;
 var score = Date.now();
 
+var deathframes = 0;
+
 (function render () {
 	a.fillStyle = "rgba(0,0,0,.3)";
 	a.fillRect(0, 0, WIDTH, HEIGHT);
@@ -19,7 +21,7 @@ var score = Date.now();
 		if (r.y >= HEIGHT) {
 			rods.splice(i, 1);
 			lives--;
-			// screen-wide flower upon life lost
+			deathframes = 60;
 		}
 	});
 	
@@ -39,13 +41,30 @@ var score = Date.now();
 		// check for and burst on collisions
 	});
 	
-	a.fillStyle = "rgba(255,255,255,.3)"
+	a.fillStyle = "rgba(255,255,255,.3)";
 	
 	a.fillText(lives + " ♥ " + ((Date.now() - score) / 1000).toFixed(2), 10, 20);
+	
+	a.fillStyle = "#FFF";
 	
 	a.beginPath();
 	a.arc(WIDTH / 2, HEIGHT, 30, 0, Math.PI, true);
 	a.fill();
+	
+	if (deathframes) {
+		a.fillStyle = "rgba(255,0,127,.3)";
+		
+		for (var i = 0; i < 12; i++) {
+			if (i % 2) {
+				a.beginPath();
+				a.moveTo(WIDTH / 2, HEIGHT / 2);
+				a.arc(WIDTH / 2, HEIGHT / 2, HEIGHT / 3, ((i / 6) + (deathframes / 240)) * Math.PI, ((i / 6) + 0.2 + (deathframes / 240)) * Math.PI, false);
+				a.fill();
+			}
+		}
+		
+		deathframes--;
+	}
 	
 	lives && requestAnimationFrame(render);
 })();

655ec985e803791687fc2f36b0f3493f1cebec9b
Teddy Cross <tkazec@gmail.com>
2013-03-31 15:36:28 -0700

    everything but bursts!!

--- js1k/flowerwarfare.js
+++ js1k/flowerwarfare.js
@@ -9,43 +9,79 @@ var score = Date.now();
 
 var deathframes = 0;
 
+Math.move = function (obj, speed) {
+	var distx = obj.tx - obj.cx;
+	var disty = obj.ty - obj.cy;
+	var angle = Math.atan2(disty, distx);
+	
+	obj.cx += speed * Math.cos(angle);
+	obj.cy += speed * Math.sin(angle);
+};
+
 (function render () {
-	a.fillStyle = "rgba(0,0,0,.3)";
-	a.fillRect(0, 0, WIDTH, HEIGHT);
+	if (!Math.floor(Math.random() * 120)) {
+		var tx = Math.floor(Math.random() * WIDTH);
+		
+		rods.push({
+			ox: tx,
+			oy: 0,
+			cx: tx,
+			cy: 0,
+			tx: Math.floor(Math.random() * WIDTH),
+			ty: HEIGHT
+		});
+	}
 	
 	rods.forEach(function (r, i) {
-		// move rod (dt, global speed)
-		// color rod (y)
-		// draw
+		Math.move(r, 0.5 + ((Date.now() - score) / 100000));
 		
-		if (r.y >= HEIGHT) {
+		a.strokeStyle = "hsl(" + (r.cy * (HEIGHT / 360)) + ",50%,50%)";
+		
+		a.beginPath();
+		a.moveTo(r.ox, r.oy);
+		a.lineTo(r.cx, r.cy);
+		a.stroke();
+		
+		if (r.cy >= HEIGHT) {
 			rods.splice(i, 1);
+			
 			lives--;
 			deathframes = 60;
 		}
 	});
 	
-	missiles.forEach(function (m) {
-		// move missile (dt)
+	missiles.forEach(function (m, i) {
+		Math.move(m, 6);
+		
 		a.strokeStyle = "#FFF";
+		
 		a.beginPath();
-		a.moveTo(m.x, m.y);
-		a.lineTo(m.j, m.k);
+		a.moveTo(WIDTH / 2, HEIGHT);
+		a.lineTo(m.cx, m.cy);
 		a.stroke();
-		// convert to bursts
+		
+		if (m.cy <= m.ty) {
+			missiles.splice(i, 1);
+			
+			bursts.push({
+				x: m.cx,
+				y: m.cy
+			});
+		}
 	});
 	
 	bursts.forEach(function (b) {
 		// size
 		// draw
 		// check for and burst on collisions
+		// (obj.x - target.x)*(obj.x - target.x) + (obj.y - target.y)*(obj.y - target.y) < rad*rad;
 	});
 	
-	a.fillStyle = "rgba(255,255,255,.3)";
+	a.fillStyle = "#FFF";
 	
 	a.fillText(lives + " ♥ " + ((Date.now() - score) / 1000).toFixed(2), 10, 20);
 	
-	a.fillStyle = "#FFF";
+	a.fillRect(0, HEIGHT - 10, WIDTH, 10)
 	
 	a.beginPath();
 	a.arc(WIDTH / 2, HEIGHT, 30, 0, Math.PI, true);
@@ -66,16 +102,17 @@ var deathframes = 0;
 		deathframes--;
 	}
 	
+	a.fillStyle = "rgba(0,0,0,.3)";
+	a.fillRect(0, 0, WIDTH, HEIGHT);
+	
 	lives && requestAnimationFrame(render);
 })();
 
 c.onclick = function (e) {
 	missiles.push({
-		x: WIDTH / 2,
-		y: HEIGHT,
-		j: e.pageX - this.offsetLeft,
-		k: e.pageY - this.offsetTop
+		cx: WIDTH / 2,
+		cy: HEIGHT,
+		tx: e.pageX - this.offsetLeft,
+		ty: e.pageY - this.offsetTop
 	});
 };
-
-// http://js1k.com/2010-first/demo/680
